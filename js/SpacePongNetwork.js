5bfc2c42af13d37da2445b4837d1d81dd43ac121
erichlof <erichlof@swbell.net>
2014-03-21 14:34:34 -0500

    Fixed current player list in top-right corner

--- js/SpacePongNetwork.js
+++ js/SpacePongNetwork.js
@@ -5,6 +5,21 @@ var chatMessage = "";
 var connectingToLobby = true;
 var chatting = false;
 var playerDivs = new Array();
+var tempDiv = null;
+var tempId = null;
+
+function initNetworkHelp(){
+	var tempDiv = document.createElement('div');
+	tempDiv.id = "networkHelp";
+	tempDiv.innerHTML = "Click on an opponent below to request a match";
+	tempDiv.style.position = "fixed";
+	tempDiv.style.top = "0%";
+	tempDiv.style.right = "2%";
+	tempDiv.style.color = "rgb(180,180,180)";
+	tempDiv.style.fontSize = "10px";
+	tempDiv.style.fontFamily = "arial";
+	document.body.appendChild(tempDiv);
+}
 
 function textEnterHandler(enter, event) {
 
@@ -42,7 +57,8 @@ function textEnterHandler(enter, event) {
 }
 
 function connectToLobby(playerName) {
-
+	
+	initNetworkHelp();
 	//init PubNub object
 	pubnub = PUBNUB.init({
 		publish_key: "pub-c-697ba66a-fbbc-49bd-a270-f96befbde57d",
@@ -64,25 +80,24 @@ function connectToLobby(playerName) {
 				myMessage = presMessage.uuid + " has entered " + channel;
 				chatLog(myMessage);
 				for (i = 0; i < playerDivs.length; i++) {
-					var tempDiv = document.getElementById(playerDivs[i].id);
-					tempDiv.innerHTML = "";
+					tempId = document.getElementById(playerDivs[i].id);
+					document.body.removeChild(tempId);
 				}
 			}
 			if (presMessage.action === 'leave') {
 				myMessage = presMessage.uuid + " has left " + channel;
 				chatLog(myMessage);
 				for (i = 0; i < playerDivs.length; i++) {
-					var tempDiv = document.getElementById(playerDivs[i].id);
-					tempDiv.innerHTML = "";
+					tempId = document.getElementById(playerDivs[i].id);
+					document.body.removeChild(tempId);
 				}
-				var tempId = document.getElementById(presMessage.uuid);
-				document.body.removeChild(tempId);
+                          	playerDivs.pop();
 			}
 			if (presMessage.action === 'timeout') {
 				myMessage = presMessage.uuid + " has timed out.";
 				chatLog(myMessage);
 			}
-			runningPresence = true; 
+			runningPresence = true;
 			presenceTimer.reset();
 		} //end presence:
 	}); //end pubnub.subscribe({
@@ -98,10 +113,10 @@ function runPresence() {
 		channel: "PongGameLobby",
 		callback: function(m) {
 			myMessage = "There are now " + m.uuids.length + " players in the lobby.";
-			chatLog(myMessage);                  //chatLog(JSON.stringify(m.uuids));
+			chatLog(myMessage);
+			//chatLog(JSON.stringify(m.uuids));
 
-			                 
-			for (i = 0; i < m.uuids.length; i++) {                       
+			for (i = 0; i < m.uuids.length; i++) {
 				playerDivs[i] = document.createElement('div');
 				playerDivs[i].id = m.uuids[i];
 				playerDivs[i].innerHTML = m.uuids[i];
@@ -109,13 +124,11 @@ function runPresence() {
 				playerDivs[i].style.top = (i + 1) * 2 + "%";
 				playerDivs[i].style.right = "2%";
 				if (m.uuids[i] === playerName)
-					playerDivs[i].style.color = "rgb(55,55,55)";
-				else playerDivs[i].style.color = "rgb(255,255,0)";
-				playerDivs[i].style.fontSize = "10px";
+					playerDivs[i].style.color = "brown";
+				else playerDivs[i].style.color = "yellow";
+				playerDivs[i].style.fontSize = "11px";
 				playerDivs[i].style.fontFamily = "arial";
 				document.body.appendChild(playerDivs[i]);
-
-				                       
 			}
 		}
 	});
@@ -139,20 +152,20 @@ function chatLog(message) {
 		pubnub.subscribe({
 			user: "",
 			callback: function (message) {
-    				console.log(opponent, ":", message);
-  			},
-  			connect: function (uuid, peerConnection) {
-    				console.log("Now connected to", uuid);
+				console.log(opponent, ":", message);
+			},
+ 			connect: function (uuid, peerConnection) {
+			console.log("Now connected to", uuid);
 				opponent = uuid;
-  			},
-  			disconnect: function (uuid, peerConnection) {
-    				console.log(opponent, "has disconnected");
-  			}
+			},
+			disconnect: function (uuid, peerConnection) {
+				console.log(opponent, "has disconnected");
+ 			}
 		});
 		
 		pubnub.publish({
-  			user: opponent, 
-  			message: [player, "says: Hello there," opponent, "- good luck!" ] 
+			user: opponent, 
+			message: [player, "says: Hello there," opponent, "- good luck!" ] 
 		});
 		
 		*/
